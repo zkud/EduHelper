fe802e5e5be5a2fa887327ebad904c4f90840755
Dmitry720 <42466047+Dmitry720@users.noreply.github.com>
2019-05-21 09:47:42 +0300

    Add delete controller

--- src/main/java/edu/bsuirDev/controllers/PlanInfoController.java
+++ src/main/java/edu/bsuirDev/controllers/PlanInfoController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -21,25 +20,40 @@ public class PlanInfoController {
     Map<String, String> planinfo(@RequestParam(value = "userid") long userid,
                                  @RequestParam(value = "planid") long planid)
     {
-        //не работает с неверным id
         UserSession userSession = new UserSession(userid);
-        Plan plan = userSession.getPlan(planid);
-
         HashMap<String, String> mapPlan = new HashMap<>();
+        if(userSession.getUser() == null) {
+            mapPlan.put("null", "null");
+            mapPlan.put("null", "null");
+            mapPlan.put("null", "null");
+            return mapPlan;
+        }
+
+        Plan plan = userSession.getPlan(planid);
+        if(plan == null) {
+            mapPlan.put("null", "null");
+            mapPlan.put("null", "null");
+            mapPlan.put("null", "null");
+            return mapPlan;
+        }
         mapPlan.put("name", plan.getInfo());
         mapPlan.put("expected result", Double.toString(plan.getResult()));
 
         HashMap<String, String> mapSteps = new HashMap<>();
         HashMap<String, String> mapOneStep = new HashMap<>();
         List<Step> list = userSession.getSteps(planid);
-        for (Step step : list) {
-            mapOneStep.put("name", step.getName());
-            mapOneStep.put("deadline", step.getDeadline().toString());
-            mapOneStep.put("cost", Double.toString(step.getCost()));
-            mapOneStep.put("complete", Boolean.toString(step.isComplete()));
-            mapSteps.put(Long.toString(step.getId()), mapOneStep.toString());
+        if(list.isEmpty()) {
+            mapPlan.put("steps", null);
+        } else {
+            for (Step step : list) {
+                mapOneStep.put("name", step.getName());
+                mapOneStep.put("deadline", step.getDeadline().toString());
+                mapOneStep.put("cost", Double.toString(step.getCost()));
+                mapOneStep.put("complete", Boolean.toString(step.isComplete()));
+                mapSteps.put(Long.toString(step.getId()), mapOneStep.toString());
+            }
+            mapPlan.put("steps", mapSteps.toString());
         }
-        mapPlan.put("steps", mapSteps.toString());
         return mapPlan;
     }
 
